732467b2c5655ca85aed170e9fbea8c7c7d70556
Adam Green <greena88@gmail.com>
2010-09-27 18:08:07 +0100

    Added extra frequency.

--- arch/arm/mach-msm/acpuclock-qsd8x50.c
+++ arch/arm/mach-msm/acpuclock-qsd8x50.c
@@ -96,6 +96,7 @@ struct clkctl_acpu_speed acpu_freq_tbl[] = {
 	{ 1036800, CCTL(CLK_TCXO, 1),		SRC_SCPLL, 0x1B, 0, 1275 },
 	{ 1075200, CCTL(CLK_TCXO, 1),		SRC_SCPLL, 0x1C, 0, 1275 },
 	{ 1113600, CCTL(CLK_TCXO, 1),		SRC_SCPLL, 0x1D, 0, 1275 },
+	{ 1152000, CCTL(CLK_TCXO, 1),		SRC_SCPLL, 0x1E, 0, 1300 },
 	{ 0 },
 };
 
